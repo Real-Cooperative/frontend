068c954a852c5bcb7a756835f133773efa3f9690
June Pratt <juneprattm@gmail.com>
2023-08-03 22:45:43 -0400

    if user context is false keep loading

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,10 +2,23 @@ import React, { useContext } from "react";
 import { UserContext } from "../../context/userContext";
 import RecipeList from "./RecipeList";
 import Account from "./Account";
+import Loading from "../Loading";
 
 const Home = () => {
     const [userContext, setUserContext] = useContext(UserContext);
-    return <>{userContext ? <RecipeList /> : <Account />}</>;
+    return (
+        <>
+            {userContext !== false ? (
+                userContext ? (
+                    <RecipeList />
+                ) : (
+                    <Account />
+                )
+            ) : (
+                <Loading />
+            )}
+        </>
+    );
 };
 
 export default Home;
